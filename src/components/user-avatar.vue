856c0f1b7094936f03889e7e56498f5205de67eb
Roman Kuba <rkuba@gitlab.com>
2020-05-31 19:32:10 +0200

    lesson6-solved

--- src/components/user-avatar.vue
+++ src/components/user-avatar.vue
@@ -1,14 +1,26 @@
-<template functional>
-  <div class="avatar">
-    <figure>
-      <img src="/avatar.png" alt="Jane Doe" />
-    </figure>
-  </div>
-</template>
-
 <script>
   export default {
     functional: true,
     name: 'UserAvatar',
+
+    render(h) {
+      // For Reference:
+      // <div class="avatar">
+      //   <figure>
+      //     <img src="/avatar.png" alt="Jane Doe" />
+      //   </figure>
+      // </div>
+
+      return h('div', { class: 'avatar' }, [
+        h('figure', [
+          h('img', {
+            attrs: {
+              src: '/avatar.png',
+              alt: 'Jane Doe',
+            },
+          }),
+        ]),
+      ]);
+    },
   };
 </script>
